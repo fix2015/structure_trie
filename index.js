97e2a2a059424dd0015f22cafe19b73cb9e3037d
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 14:14:29 +0000

    Create task trie

---
+++ index.js
@@ -0,0 +1,35 @@
+class TrieNode {
+    constructor() {
+       
+    }
+}
+
+class Trie {
+    constructor() {
+        
+    }
+
+    // Insert a word into the trie
+    insert(word) {
+       
+    }
+
+    // Search for a word in the trie
+    search(word) {
+       
+    }
+
+    // Check if any word in the trie starts with the given prefix
+    startsWith(prefix) {
+        
+    }
+
+    // Delete a word from the trie
+    delete(word) {
+       
+    }
+
+    // Helper function to delete a word
+    deleteHelper(node, word, index) {
+        
+    }
